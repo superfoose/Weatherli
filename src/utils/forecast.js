7c234b045b3eb9c5d1b9b5592db12a9c84787cf7
superfoose <davidlevgabbay@gmail.com>
2022-09-07 18:10:26 +0300

    adding weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+            callback(undefined, 
+            body.current.weather_descriptions[0] + ". It is currently " + 
+            body.current.temperature + " degress out." + " And it feels like " + body.current.feelslike + 
+            " degrees. And the humidity is " + body.current.humidity + "%.")
         }
     })
 }
